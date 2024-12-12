783cfbdfb50447a6c9dc0724443d1ae6a7bea47e
kingmoyce <kingandymoyce1@gmail.com>
2024-12-12 17:22:53 +0000

    deployment

--- src/data/productData.js
+++ src/data/productData.js
@@ -5,7 +5,7 @@ export const products = [
     brand: "CameraPro",
     description: "High-resolution digital camera for photography enthusiasts",
     price: 299999,
-    image: "/images/camera.jpg"
+    image: "https://i.pinimg.com/236x/0a/95/73/0a957350d8ba1c3e0fb805d610269154.jpg"
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ export const products = [
     brand: "TechVision",
     description: "144Hz gaming monitor with HDR support",
     price: 159999,
-    image: "/images/monitor.jpg"
+    image: "https://i.pinimg.com/236x/d0/5d/2a/d05d2ac39f93f8a0ed379a9229669b6c.jpg"
   },
   {
     id: 3,
@@ -21,14 +21,14 @@ export const products = [
     brand: "TypeMaster",
     description: "RGB mechanical keyboard with custom switches",
     price: 45999,
-    image: "/images/keyboard.jpg"
+    image: "https://i.pinimg.com/736x/bd/5a/2b/bd5a2b62693e5b37784fbfc6a5169624.jpg"
   },
   {
     id: 4,
     name: "Wireless Mouse",
     brand: "ClickPro",
-    description: "High-precision wireless gaming mouse",
+    description: "https://i.pinimg.com/236x/ac/65/a5/ac65a581cb5b83062b9f3af05bf068fd.jpg",
     price: 29999,
-    image: "/images/mouse.jpg"
+    image: "./images/mouse.jpg"
   }
 ];
