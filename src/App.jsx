94407cc2e135e6cdfdae2eab7f1424e679938c82
kingmoyce <kingandymoyce1@gmail.com>
2024-12-14 11:18:47 +0000

    update deployment

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
             <Routes>
               <Route path="/" element={<ProductList />} />
               <Route path="/cart" element={<Cart />} />
+              <Route path="*" element={<ProductList />} />
             </Routes>
           </main>
         </div>
